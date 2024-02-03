27d223044aa10971511fde7d1830bee65514236a
Pavel Kononov <pro100cuber@gmail.com>
2024-02-03 16:40:43 +0700

    tests: added datetime parse\serialize

--- tests/DataTransferObjectSerializeTest.php
+++ tests/DataTransferObjectSerializeTest.php
@@ -42,7 +42,7 @@ class DataTransferObjectSerializeTest extends \PHPUnit\Framework\TestCase
             #[PropertyMapFrom('from')]
             #[PropertyMapTo('to')]
             #[PropertySerializeArray(InternalClass2::class, 'serialize', true, InternalClass2::class)]
-            #[PropertyParseArray(InternalClass2::class,'wrap',true)]
+            #[PropertyParseArray(InternalClass2::class, 'wrap', true)]
             public array $property;
         };
         $test = ['from' => [['test' => 5]]];
@@ -50,38 +50,24 @@ class DataTransferObjectSerializeTest extends \PHPUnit\Framework\TestCase
         $this->assertSame(5, $item->property[0]->test);
         $this->assertSame(5, $item->toArray()['to'][0] ?? null);
     }
-//
-//    public function test_parse_dto_array()
-//    {
-//        $item = new class extends DataTransferObject {
-//            #[PropertyParseArray(InternalClass::class, 'wrap', true)]
-//            public array $property;
-//        };
-//        $test = ['property' => [['test' => 5]]];
-//        $item->fill($test);
-//        $this->assertSame(5, $item->property[0]->test);
-//        $this->assertSame(5, $item->toArray()['property'][0]['test'] ?? null);
-//    }
-//
-//    public function test_parse_datetime()
-//    {
-//        $item = new class extends DataTransferObject {
-//            #[PropertyParse(DateTime::class)]
-//            public DateTime $property;
-//        };
-//        $test = ['property' => '20230701T150000'];
-//        $item->fill($test);
-//        $this->assertSame('20230701T150000', $item->property->format('Ymd\THis'));
-//    }
-//
-//    public function test_parse_datetime_array()
-//    {
-//        $item = new class extends DataTransferObject {
-//            #[PropertyParseArray(DateTime::class)]
-//            public array $property;
-//        };
-//        $test = ['property' => ['20230701T150000']];
-//        $item->fill($test);
-//        $this->assertSame('20230701T150000', $item->property[0]->format('Ymd\THis'));
-//    }
+
+    public function test_serialize_datetime()
+    {
+        $item = new class extends DataTransferObject {
+            #[PropertyMapFrom('from')]
+            #[PropertyMapTo('to')]
+            #[PropertySerialize(self::class, 'toDateTime', true, DateTime::class)]
+            #[PropertyParse(DateTime::class)]
+            public DateTime $property;
+
+            public static function toDateTime($i)
+            {
+                return $i->format('Ymd\THis');
+            }
+        };
+        $test = ['from' => '20230701T100000'];
+        $item->fill($test);
+        $this->assertSame('20230701T100000', $item->property->format('Ymd\THis'));
+        $this->assertSame('20230701T100000', $item->toArray()['to']);
+    }
 }
